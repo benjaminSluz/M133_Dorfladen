753e317cf2f918191cfb8b8e8910ce17e96ce670
Benjamin Schleiss <Benjamin_Schleiss@sluz.ch>
2022-01-06 14:30:55 +0100

    Made base for ejs

---
+++ deps.js
@@ -0,0 +1,9 @@
+"use strict";
+export {
+    Router,
+    send,
+    Application,
+}
+from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export { renderFileToString }
+from "https://deno.land/x/dejs@0.9.3/mod.ts";
